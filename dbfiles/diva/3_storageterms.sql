6e7b6fa19d7af324c29061414d8f044de430d200
olovm <olov.mckie@ub.uu.se>
2022-12-02 15:24:09 +0000

    adding diva db files

---
+++ dbfiles/diva/3_storageterms.sql
@@ -0,0 +1,4 @@
+COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey) FROM stdin;
+coraUser	coraUser:4412982402853626	userUserIdStorageTerm	divauser@cora.epc.ub.uu.se	userId
+coraUser	coraUser:5368656924943436	userUserIdStorageTerm	divaguest@system.cora.uu.se	userId
+systemOneUser	161616	userUserIdStorageTerm	divauser@cora.epc.ub.uu.se	userId
---
+++ dbfiles/divaTestSystem/3_storageterms.sql
@@ -0,0 +1,6 @@
+COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey) FROM stdin;
+coraUser	coraUser:491201365536105	userUserIdStorageTerm	domainAdminKTH@diva.cora.uu.se	userId
+coraUser	coraUser:491144693381458	userUserIdStorageTerm	dominAdminUU@diva.cora.uu.se	userId
+coraUser	coraUser:490742519075086	userUserIdStorageTerm	divaEverything@diva.cora.uu.se	userId
+coraUser	coraUser:491055276494310	userUserIdStorageTerm	systemAdmin@diva.cora.uu.se	userId
+coraUser	coraUser:1299694997493014	userUserIdStorageTerm	divaFitnesseEverything@system.cora.uu.se	userId
