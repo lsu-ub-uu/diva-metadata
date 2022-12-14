960320e53bd0ef05e390b6d90104d0922c71b98f
lsu <lsu@b396dc4acf0d>
2022-12-14 13:32:19 +0000

    change export script from docker

--- export.sh
+++ export.sh
@@ -8,7 +8,9 @@ start(){
 	echo "Exporting data from docker "$dockerName
 	docker exec  -i $dockerName bash < runInsideDocker.sh
 	echo "Copying files to local export folder..."
-	docker cp $dockerName:/$baseDir .
+	docker cp $dockerName:/$baseDir/diva .
+	docker cp $dockerName:/$baseDir/divaProdSystem .
+	docker cp $dockerName:/$baseDir/divaTestSystem .
 }
 
 start
