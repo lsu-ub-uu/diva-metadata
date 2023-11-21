20658336f593410c0da0dbe743bc32f12daca1ed
DiVA Support <diva-support@ub.uu.se>
2023-11-21 14:42:00 +0100

    DbFiles updated tis 21 nov 2023 14:42:00 CET

---
+++ dbfiles/divaClient/2_links.sql
@@ -0,0 +1,5 @@
+COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
+system	divaClient	recordType	system
+system	divaClient	system	divaClient
+system	divaClient	user	coraUser:490742519075086
+system	divaClient	validationType	system
