7abe9fa75acefcc8708413102b72d3568209affa
DiVA Support <diva-support@ub.uu.se>
2023-11-28 16:18:03 +0100

    DbFiles updated tis 28 nov 2023 16:18:03 CET

--- dbfiles/divaClient/2_links.sql
+++ dbfiles/divaClient/2_links.sql
@@ -3,10 +3,10 @@ system	divaClient	recordType	system
 system	divaClient	system	divaClient
 system	divaClient	user	coraUser:490742519075086
 system	divaClient	validationType	system
-text	divaClient_LoginButtonText	recordType	text
-text	divaClient_LoginButtonText	system	divaClient
-text	divaClient_LoginButtonText	user	161616
-text	divaClient_LoginButtonText	validationType	coraText
+text	divaClient_LoginText	recordType	text
+text	divaClient_LoginText	system	divaClient
+text	divaClient_LoginText	user	161616
+text	divaClient_LoginText	validationType	coraText
 text	divaClient_ResetButtonText	recordType	text
 text	divaClient_ResetButtonText	system	divaClient
 text	divaClient_ResetButtonText	user	161616
