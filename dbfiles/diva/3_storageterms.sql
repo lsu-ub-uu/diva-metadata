3bc4d631675ae75a34a1b63b967e1e74c91f58ac
olovm <olov.mckie@ub.uu.se>
2024-02-27 09:58:26 +0000

    Start work on clearing divaData

---
+++ dbfiles/diva/3_storageterms.sql
@@ -0,0 +1,4 @@
+COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey) FROM stdin;
+user	161616	userUserIdStorageTerm	divauser@cora.epc.ub.uu.se	userId
+user	coraUser:4412982402853626	userUserIdStorageTerm	divauser@cora.epc.ub.uu.se	userId
+user	coraUser:5368656924943436	userUserIdStorageTerm	divaguest@system.cora.uu.se	userId
