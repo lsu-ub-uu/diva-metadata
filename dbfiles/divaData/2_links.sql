a222ca559a143e49285bb27e1ec71668ff9b1bc6
DiVA Support <diva-support@ub.uu.se>
2024-04-26 16:22:54 +0200

    DbFiles updated Fri Apr 26 04:22:54 PM CEST 2024

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -1,9 +1,9 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
-divaOutput	divaOutput:12752429327499596	recordType	divaOutput
-divaOutput	divaOutput:12752429327499596	system	divaData
-divaOutput	divaOutput:12752429327499596	user	161616
-divaOutput	divaOutput:12752429327499596	validationType	divaOutput
-divaOutput	divaOutput:12752846950777138	recordType	divaOutput
-divaOutput	divaOutput:12752846950777138	system	divaData
-divaOutput	divaOutput:12752846950777138	user	161616
-divaOutput	divaOutput:12752846950777138	validationType	divaOutput
+divaOutput	divaOutput:12756234035782657	recordType	divaOutput
+divaOutput	divaOutput:12756234035782657	system	divaData
+divaOutput	divaOutput:12756234035782657	user	161616
+divaOutput	divaOutput:12756234035782657	validationType	preprint
+divaOutput	divaOutput:12756272669815950	recordType	divaOutput
+divaOutput	divaOutput:12756272669815950	system	divaData
+divaOutput	divaOutput:12756272669815950	user	161616
+divaOutput	divaOutput:12756272669815950	validationType	divaOutput
