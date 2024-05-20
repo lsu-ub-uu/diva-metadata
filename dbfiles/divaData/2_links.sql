56ae56a41ad2c6fb729856c92623c446cae1a25e
DiVA Support <diva-support@ub.uu.se>
2024-05-20 16:35:13 +0200

    DbFiles updated Mon May 20 04:35:13 PM CEST 2024

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -3,3 +3,11 @@ divaOutput	divaOutput:14499010602713105	recordType	divaOutput
 divaOutput	divaOutput:14499010602713105	system	divaData
 divaOutput	divaOutput:14499010602713105	user	161616
 divaOutput	divaOutput:14499010602713105	validationType	divaOutput
+publisher	ett	recordType	publisher
+publisher	ett	system	divaData
+publisher	ett	user	161616
+publisher	ett	validationType	publisher
+publisher	someId1	recordType	publisher
+publisher	someId1	system	divaData
+publisher	someId1	user	161616
+publisher	someId1	validationType	publisher
