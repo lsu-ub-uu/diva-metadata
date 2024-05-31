80f88a1d48587325df85b1124c58264f48a7026e
DiVA Support <diva-support@ub.uu.se>
2024-05-31 16:56:26 +0200

    DbFiles updated Fri May 31 04:56:26 PM CEST 2024

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -1,1 +1,9 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
+publisher	123	recordType	publisher
+publisher	123	system	divaData
+publisher	123	user	161616
+publisher	123	validationType	publisher
+publisher	1234	recordType	publisher
+publisher	1234	system	divaData
+publisher	1234	user	161616
+publisher	1234	validationType	publisher
