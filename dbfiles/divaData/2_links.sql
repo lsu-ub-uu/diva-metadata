fc1ac0b6c390de93b993b295546d32ca70ae55c4
DiVA Support <diva-support@ub.uu.se>
2024-10-08 12:08:23 +0200

    DbFiles updated Tue Oct  8 12:08:23 PM CEST 2024

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -1,5 +1,13 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
+diva-journal	12	recordType	diva-journal
+diva-journal	12	system	divaData
+diva-journal	12	user	161616
+diva-journal	12	validationType	diva-journal
 divaOutputSwepub	divaOutputSwepub:257396145108349	recordType	divaOutputSwepub
 divaOutputSwepub	divaOutputSwepub:257396145108349	system	divaData
 divaOutputSwepub	divaOutputSwepub:257396145108349	user	161616
 divaOutputSwepub	divaOutputSwepub:257396145108349	validationType	divaOutputSwepub
+divaOutputSwepub	divaOutputSwepub:588620549296863	recordType	divaOutputSwepub
+divaOutputSwepub	divaOutputSwepub:588620549296863	system	divaData
+divaOutputSwepub	divaOutputSwepub:588620549296863	user	161616
+divaOutputSwepub	divaOutputSwepub:588620549296863	validationType	divaOutputSwepub
