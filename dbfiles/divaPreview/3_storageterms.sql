1221aae35299af70fb783c3faac21a116b2af44f
DiVA Support <diva-support@ub.uu.se>
2024-10-16 15:05:00 +0200

    DbFiles updated ons 16 okt 2024 15:05:00 CEST

--- dbfiles/divaPreview/3_storageterms.sql
+++ dbfiles/divaPreview/3_storageterms.sql
@@ -1,19 +1,10 @@
 COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey) FROM stdin;
-user	coraUser:1299694997493014	userUserIdStorageTerm	divaFitnesseEverything@system.cora.uu.se	userId
 user	coraUser:1299694997493014	userLoginIdStorageTerm	divaFitnesseEverything@system.cora.uu.se	loginId
 user	coraUser:4412982402853626	userLoginIdStorageTerm	divaUser@cora.epc.ub.uu.se	loginId
-user	coraUser:4412982402853626	userUserIdStorageTerm	divaUser@cora.epc.ub.uu.se	userId
-user	coraUser:490742519075086	userUserIdStorageTerm	divaEverything@diva.cora.uu.se	userId
 user	coraUser:490742519075086	userLoginIdStorageTerm	divaEverything@diva.cora.uu.se	loginId
 user	coraUser:491055276494310	userLoginIdStorageTerm	systemAdmin@diva.cora.uu.se	loginId
-user	coraUser:491055276494310	userUserIdStorageTerm	systemAdmin@diva.cora.uu.se	userId
-user	coraUser:491144693381458	userUserIdStorageTerm	dominAdminUU@diva.cora.uu.se	userId
 user	coraUser:491144693381458	userLoginIdStorageTerm	dominAdminUU@diva.cora.uu.se	loginId
-user	coraUser:491201365536105	userUserIdStorageTerm	domainAdminKTH@diva.cora.uu.se	userId
 user	coraUser:491201365536105	userLoginIdStorageTerm	domainAdminKTH@diva.cora.uu.se	loginId
-user	coraUser:542161975513761	userUserIdStorageTerm	fitnesseSystemAdmin@user.uu.se	userId
 user	coraUser:542161975513761	userLoginIdStorageTerm	fitnesseSystemAdmin@user.uu.se	loginId
 user	coraUser:542165753711000	userLoginIdStorageTerm	fitnesseDomainAdminUU@user.uu.se	loginId
-user	coraUser:542165753711000	userUserIdStorageTerm	fitnesseDomainAdminUU@user.uu.se	userId
-user	coraUser:542168680454562	userUserIdStorageTerm	fitnesseDomainAdminKTH@user.kth.se	userId
 user	coraUser:542168680454562	userLoginIdStorageTerm	fitnesseDomainAdminKTH@user.kth.se	loginId
