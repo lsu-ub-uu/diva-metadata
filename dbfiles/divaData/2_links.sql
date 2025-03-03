7dee85fbe0d9548a981e8778542f9482551b3417
DiVA Support <diva-support@ub.uu.se>
2025-03-03 15:12:38 +0100

    DbFiles updated Mon Mar  3 03:12:38 PM CET 2025

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -1,1 +1,10 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
+diva-organisation	diva-organisation:1806079719872542	recordType	diva-organisation
+diva-organisation	diva-organisation:1806079719872542	system	divaData
+diva-organisation	diva-organisation:1806079719872542	user	161616
+diva-organisation	diva-organisation:1806079719872542	validationType	diva-topOrganisation
+diva-organisation	diva-organisation:1806137829257774	diva-organisation	diva-organisation:1806079719872542
+diva-organisation	diva-organisation:1806137829257774	recordType	diva-organisation
+diva-organisation	diva-organisation:1806137829257774	system	divaData
+diva-organisation	diva-organisation:1806137829257774	user	161616
+diva-organisation	diva-organisation:1806137829257774	validationType	diva-partOfOrganisation
