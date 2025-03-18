ac6ae48b44c96bcaf62dd81750553f4c2a4b7ed9
DiVA Support <diva-support@ub.uu.se>
2025-03-18 11:12:35 +0100

    DbFiles updated tis 18 mar 2025 11:12:35 CET

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -1,11 +1,1 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
-binary	binary:3002869564236714	recordType	binary
-binary	binary:3002869564236714	system	divaData
-binary	binary:3002869564236714	user	141414
-binary	binary:3002869564236714	user	161616
-binary	binary:3002869564236714	validationType	genericBinary
-diva-output	diva-output:3002859040134435	binary	binary:3002869564236714
-diva-output	diva-output:3002859040134435	recordType	diva-output
-diva-output	diva-output:3002859040134435	system	divaData
-diva-output	diva-output:3002859040134435	user	161616
-diva-output	diva-output:3002859040134435	validationType	publication_report
