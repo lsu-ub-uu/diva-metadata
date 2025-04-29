a554957540586d7925fe51c53922151a6abad03a
DiVA Support <diva-support@ub.uu.se>
2025-04-29 15:55:38 +0200

    DbFiles updated Tue Apr 29 03:55:38 PM CEST 2025

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -4,11 +4,6 @@ diva-organisation	diva-organisation:6709999935654468	recordType	diva-organisatio
 diva-organisation	diva-organisation:6709999935654468	system	divaData
 diva-organisation	diva-organisation:6709999935654468	user	161616
 diva-organisation	diva-organisation:6709999935654468	validationType	diva-topOrganisation
-diva-output	diva-output:6727055294077429	permissionUnit	uu
-diva-output	diva-output:6727055294077429	recordType	diva-output
-diva-output	diva-output:6727055294077429	system	divaData
-diva-output	diva-output:6727055294077429	user	161616
-diva-output	diva-output:6727055294077429	validationType	diva_degree-project
 diva-person	444	diva-organisation	diva-organisation:1530219071900116
 diva-person	444	recordType	diva-person
 diva-person	444	system	divaData
