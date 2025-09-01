51c87169d62fa042907425ed2af49f3f9f74f7d2
DiVA Support <diva-support@ub.uu.se>
2025-09-01 14:41:32 +0200

    DbFiles updated mån  1 sep 2025 14:41:32 CEST

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -1,6 +1,1 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
-diva-output	diva-output:103914839507573	permissionUnit	uu
-diva-output	diva-output:103914839507573	recordType	diva-output
-diva-output	diva-output:103914839507573	system	divaData
-diva-output	diva-output:103914839507573	user	161616
-diva-output	diva-output:103914839507573	validationType	publication_other
