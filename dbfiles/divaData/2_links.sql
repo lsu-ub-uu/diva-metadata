24521cfb4af52de0608e1e27a03b5f0751ee5431
DiVA Support <diva-support@ub.uu.se>
2025-10-07 16:13:19 +0200

    DbFiles updated Tue Oct  7 04:13:19 PM CEST 2025

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -1,5 +1,1 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
-diva-person	diva-person:6587666474789668	recordType	diva-person
-diva-person	diva-person:6587666474789668	system	divaData
-diva-person	diva-person:6587666474789668	user	161616
-diva-person	diva-person:6587666474789668	validationType	diva-person
