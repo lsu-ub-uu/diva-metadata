7345d6f3bf823202b8455184c7b21ff2606a92f9
DiVA Support <diva-support@ub.uu.se>
2025-10-14 09:15:47 +0200

    DbFiles updated Tue Oct 14 09:15:47 AM CEST 2025

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -1,13 +1,1 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
-diva-theme	varldskulturmuseerna	loginUnit	divaPasswordLoginUnit
-diva-theme	varldskulturmuseerna	permissionUnit	varldskulturmuseerna
-diva-theme	varldskulturmuseerna	recordType	diva-theme
-diva-theme	varldskulturmuseerna	system	divaData
-diva-theme	varldskulturmuseerna	user	161616
-diva-theme	varldskulturmuseerna	validationType	diva-theme
-diva-theme	varldskulturmuseerna3-theme	loginUnit	divaPasswordLoginUnit
-diva-theme	varldskulturmuseerna3-theme	permissionUnit	varldskulturmuseerna
-diva-theme	varldskulturmuseerna3-theme	recordType	diva-theme
-diva-theme	varldskulturmuseerna3-theme	system	divaData
-diva-theme	varldskulturmuseerna3-theme	user	141414
-diva-theme	varldskulturmuseerna3-theme	validationType	diva-theme
--- dbfiles/divaData/3_storageterms.sql
+++ dbfiles/divaData/3_storageterms.sql
@@ -1,5 +1,1 @@
 COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey) FROM stdin;
-diva-theme	varldskulturmuseerna	themeHostnameStorageTerm	varldskulturmssssuseerna.localhost	hostname
-diva-theme	varldskulturmuseerna3-theme	themeHostnameStorageTerm	varldskulturmuse3erna.localhost	hostname
-diva-theme	varldskulturmuseerna3-theme	themeHostnameStorageTerm	varldskulturmuseer3na.cora.epc.ub.uu.se	hostname
-diva-theme	varldskulturmuseerna3-theme	themeHostnameStorageTerm	varldskulturmusee3rna.pre.diva-portal.org	hostname
