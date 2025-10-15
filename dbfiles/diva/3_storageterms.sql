fa19a78ff4b2e4afb90204fb4c90086e87fb73c0
DiVA Support <diva-support@ub.uu.se>
2025-10-15 16:21:17 +0200

    DbFiles updated Wed Oct 15 04:21:17 PM CEST 2025

--- dbfiles/diva/3_storageterms.sql
+++ dbfiles/diva/3_storageterms.sql
@@ -3,8 +3,8 @@ diva-member	diva	memberHostnameStorageTerm	localhost	hostname
 diva-member	diva	memberHostnameStorageTerm	diva.portal.org	hostname
 diva-member	diva	memberHostnameStorageTerm	pre.diva-portal.org	hostname
 diva-member	diva	memberHostnameStorageTerm	preview.diva.cora.epc.ub.uu.se	hostname
-diva-member	varldskulturmuseerna	memberHostnameStorageTerm	varldskulturmuseerna.pre.diva-portal.org	hostname
 diva-member	varldskulturmuseerna	memberHostnameStorageTerm	varldskulturmuseerna.localhost	hostname
+diva-member	varldskulturmuseerna	memberHostnameStorageTerm	varldskulturmuseerna.pre.diva-portal.org	hostname
 diva-member	varldskulturmuseerna	memberHostnameStorageTerm	varldskulturmuseerna.preview.diva.cora.epc.ub.uu.se	hostname
 user	161616	userLoginIdStorageTerm	divaAdmin@cora.epc.ub.uu.se	loginId
 user	coraUser:5368656924943436	userLoginIdStorageTerm	divaguest@system.cora.uu.se	loginId
@@ -14,10 +14,7 @@ user	user:15433868478865974	userLoginIdStorageTerm	kenan399@user.uu.se	loginId
 user	user:15433925923832105	userLoginIdStorageTerm	egisw290@user.uu.se	loginId
 user	user:15433988457679920	userLoginIdStorageTerm	andma210@user.uu.se	loginId
 user	user:15434036318237201	userLoginIdStorageTerm	marvi707@user.uu.se	loginId
-user	user:15434061862856387	userLoginIdStorageTerm	evaag112@user.uu.se	loginId
-user	user:15434085053834213	userLoginIdStorageTerm	danfl648@user.uu.se	loginId
 user	user:15434132643259965	userLoginIdStorageTerm	palra847@user.uu.se	loginId
 user	user:15434154245590545	userLoginIdStorageTerm	olfel499@user.uu.se	loginId
 user	user:15434188209615056	userLoginIdStorageTerm	stand516@user.uu.se	loginId
-user	user:8295758208335884	userLoginIdStorageTerm	helena.rundkrantz@varldskulturmuseerna.se	loginId
 user	user:9091016887765767	userLoginIdStorageTerm	leoda622@user.uu.se	loginId
--- dbfiles/divaPreview/3_storageterms.sql
+++ dbfiles/divaPreview/3_storageterms.sql
@@ -2,10 +2,9 @@ COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey)
 user	coraUser:1299694997493014	userLoginIdStorageTerm	divaFitnesseEverything@system.cora.uu.se	loginId
 user	coraUser:4412982402853626	userLoginIdStorageTerm	divaUser@cora.epc.ub.uu.se	loginId
 user	coraUser:490742519075086	userLoginIdStorageTerm	divaEverything@diva.cora.uu.se	loginId
-user	coraUser:491055276494310	userLoginIdStorageTerm	systemAdmin@diva.cora.uu.se	loginId
 user	coraUser:491144693381458	userLoginIdStorageTerm	dominAdminUU@diva.cora.uu.se	loginId
 user	coraUser:491201365536105	userLoginIdStorageTerm	domainAdminKTH@diva.cora.uu.se	loginId
 user	coraUser:542161975513761	userLoginIdStorageTerm	fitnesseSystemAdmin@user.uu.se	loginId
 user	coraUser:542165753711000	userLoginIdStorageTerm	fitnesseDomainAdminUU@user.uu.se	loginId
 user	coraUser:542168680454562	userLoginIdStorageTerm	fitnesseDomainAdminKTH@user.kth.se	loginId
-user	user:8985188367779791	userLoginIdStorageTerm	domainAdminVarldskulturmuseerna@diva.cora.uu.se	loginId
+user	user:8985188367779791	userLoginIdStorageTerm	domainAdminNordiskaMuseet@diva.cora.uu.se	loginId
