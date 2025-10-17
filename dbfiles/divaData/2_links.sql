b6a8168f0c1e92530d276b2013551d08346c145f
DiVA Support <diva-support@ub.uu.se>
2025-10-17 15:28:30 +0200

    DbFiles updated Fri Oct 17 03:28:30 PM CEST 2025

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -1,12 +1,1 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
-binary	binary:9754168985836417	recordType	binary
-binary	binary:9754168985836417	system	divaData
-binary	binary:9754168985836417	user	141414
-binary	binary:9754168985836417	user	161616
-binary	binary:9754168985836417	validationType	genericBinary
-diva-output	diva-output:9754154999108322	binary	binary:9754168985836417
-diva-output	diva-output:9754154999108322	permissionUnit	nordiskamuseet
-diva-output	diva-output:9754154999108322	recordType	diva-output
-diva-output	diva-output:9754154999108322	system	divaData
-diva-output	diva-output:9754154999108322	user	161616
-diva-output	diva-output:9754154999108322	validationType	publication_report
