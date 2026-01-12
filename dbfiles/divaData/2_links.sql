a30e489ed8638b95d8803c4c150def327e19f3f8
DiVA Support <diva-support@ub.uu.se>
2026-01-12 16:21:02 +0100

    DbFiles updated Mon Jan 12 04:21:02 PM CET 2026

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -1,1 +1,6 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
+diva-subject	9	permissionUnit	uu
+diva-subject	9	recordType	diva-subject
+diva-subject	9	system	divaData
+diva-subject	9	user	161616
+diva-subject	9	validationType	diva-subject
