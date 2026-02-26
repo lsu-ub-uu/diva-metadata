5a6fd276e8284313d16a8fa0193fc1f214bd1261
DiVA Support <diva-support@ub.uu.se>
2026-02-26 14:49:59 +0100

    DbFiles updated Thu Feb 26 02:49:59 PM CET 2026

--- dbfiles/divaData/3_storageterms.sql
+++ dbfiles/divaData/3_storageterms.sql
@@ -1,1 +1,3 @@
 COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey) FROM stdin;
+diva-funder	4	identifierDoiStorageTerm	10.12323435/adasdasd	identifierDoi
+diva-funder	4	identifierRorStorageTerm	048a87296	identifierRor
