947e763878431c27459d53435fb471d86e10038f
DiVA Support <diva-support@ub.uu.se>
2026-02-27 17:47:18 +0100

    DbFiles updated Fri Feb 27 05:47:18 PM CET 2026

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -1,1 +1,6 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
+diva-course	3	permissionUnit	uu
+diva-course	3	recordType	diva-course
+diva-course	3	system	divaData
+diva-course	3	user	161616
+diva-course	3	validationType	diva-course
