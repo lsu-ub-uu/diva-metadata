414ce9238bdf4ab55df551af6c38a58bc236c432
DiVA Support <diva-support@ub.uu.se>
2026-02-27 15:26:35 +0100

    DbFiles updated Fri Feb 27 03:26:35 PM CET 2026

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -1,5 +1,1 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
-diva-funder	4	recordType	diva-funder
-diva-funder	4	system	divaData
-diva-funder	4	user	161616
-diva-funder	4	validationType	diva-funder
--- dbfiles/divaData/3_storageterms.sql
+++ dbfiles/divaData/3_storageterms.sql
@@ -1,3 +1,1 @@
 COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey) FROM stdin;
-diva-funder	4	identifierDoiStorageTerm	10.12323435/adasdasd	identifierDoi
-diva-funder	4	identifierRorStorageTerm	048a87296	identifierRor
