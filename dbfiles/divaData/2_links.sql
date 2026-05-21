7136f7d4603838688bc603bc7eb17a3f33d2aa14
DiVA Support <diva-support@ub.uu.se>
2026-05-21 17:05:49 +0200

    DbFiles updated Thu May 21 05:05:49 PM CEST 2026

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -1,1 +1,6 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
+diva-output	35	permissionUnit	nordiskamuseet
+diva-output	35	recordType	diva-output
+diva-output	35	system	divaData
+diva-output	35	user	161616
+diva-output	35	validationType	publication_report
