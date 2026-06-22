7356a44996788af3e6dbd1c1799f68aaf78928e6
DiVA Support <diva-support@ub.uu.se>
2026-06-22 11:58:35 +0200

    DbFiles updated Mon Jun 22 11:58:35 AM CEST 2026

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -1,1 +1,6 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
+diva-localLabel	10	permissionUnit	uu
+diva-localLabel	10	recordType	diva-localLabel
+diva-localLabel	10	system	divaData
+diva-localLabel	10	user	161616
+diva-localLabel	10	validationType	diva-localLabel
