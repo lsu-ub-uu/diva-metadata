757e78443675bbc3352f7c89de6a6f493cecd1f6
DiVA Support <diva-support@ub.uu.se>
2026-09-24 11:11:13 +0200

    DbFiles updated Thu Sep 24 11:11:13 AM CEST 2026

--- dbfiles/divaData/2_links.sql
+++ dbfiles/divaData/2_links.sql
@@ -1,1 +1,9 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
+diva-oldRecord	5858	recordType	diva-oldRecord
+diva-oldRecord	5858	system	divaData
+diva-oldRecord	5858	user	161616
+diva-oldRecord	5858	validationType	diva-oldRecord
+diva-oldRecord	59595	recordType	diva-oldRecord
+diva-oldRecord	59595	system	divaData
+diva-oldRecord	59595	user	161616
+diva-oldRecord	59595	validationType	diva-oldRecord
--- dbfiles/divaData/3_storageterms.sql
+++ dbfiles/divaData/3_storageterms.sql
@@ -1,1 +1,3 @@
 COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey) FROM stdin;
+diva-oldRecord	5858	oldIdStorageTerm	99999	oldId
+diva-oldRecord	59595	oldIdStorageTerm	8989	oldId
